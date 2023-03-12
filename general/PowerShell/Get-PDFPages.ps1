b36f7eb144d4f9839eaa398fbb81b9be6a5371e2
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-03-12 12:18:56 +0800

    Get PDF Page Count

---
+++ general/PowerShell/Get-PDFPages.ps1
@@ -0,0 +1,38 @@
+param(
+    [Parameter(HelpMessage="The folder to scan where the PDF files are located.")]
+    $folderPathToScan,
+
+    [Parameter(HelpMessage="The folder to scan where the PDF files are located.")]
+    $output
+)
+
+function main() {
+    $pdfFiles = Get-ChildItem -Path $folderPathToScan -Filter *.pdf -Recurse
+
+    $totalPages = 0
+    $totalFiles = 0
+
+    $files = @()
+
+    foreach($file in $pdfFiles){
+        $pdfFileTotalPage = (c:\scripts\pdfinfo $file.FullName | Select-String -Pattern '(?<=Pages:\s*)\d+').Matches.Value
+
+        $totalPages++
+        $totalFiles++
+
+        $thisPDFFileObject = New-Object -TypeName PSObject
+        $thisPDFFileObject | Add-Member -NotePropertyName PDFFile -NotePropertyValue $file.FullName
+        $thisPDFFileObject | Add-Member -NotePropertyName Pages -NotePropertyValue $pdfFileTotalPage
+
+        $files += $thisPDFFileObject
+    }
+
+    if ($output) {
+        $files | Export-Csv -Path $output 
+    }
+
+    Write-Output "Total Number of pages $($totalPages) for all $($totalFiles) PDF Files."
+    $files | FT -AutoSize
+}
+
+main
