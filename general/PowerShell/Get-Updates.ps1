f28589a51cfd07739fb2b3d0e6491ea2d77d13a6
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-06-18 15:30:41 +0800

    Added message if reboot is required

--- general/PowerShell/Get-Updates.ps1
+++ general/PowerShell/Get-Updates.ps1
@@ -1,13 +1,40 @@
 param ($KBNumber)
 
-Install-Module PSWindowsUpdate -AllowClobber
+$PSVersion = host
+if ($PSVersion.version > 4) {
+  Install-Module PSWindowsUpdate -AllowClobber
 
-$Update = Get-WUList -KBArticleID $KBNumber
-If ($Update) {
-    Write-Output "Update found: $($Update.Title)"
-    $DownloadedUpdate = $Update | Get-WUDownload
-    #$DownloadedUpdate | Install-WUUpdate -AcceptAll -AutoReboot
-    $DownloadedUpdate
-} Else {
-    Write-Output "No update found with KB number: $KBNumber"
+  $Update = Get-WUList -KBArticleID $KBNumber
+  If ($Update) {
+      Write-Output "Update found: $($Update.Title)"
+      $DownloadedUpdate = $Update | Get-WUDownload
+      #$DownloadedUpdate | Install-WUUpdate -AcceptAll -AutoReboot
+      $DownloadedUpdate
+  } Else {
+      Write-Output "No update found with KB number: $KBNumber"
+  }
+} else {
+  $Criteria = "IsInstalled=0 and Type='Software' and IsHidden=0"
+  $Searcher = New-Object -ComObject Microsoft.Update.Searcher
+  $SearchResult = $Searcher.Search($Criteria).Updates
+
+  $Update = $SearchResult | Where-Object {$_.KBArticleIDs -contains $KBNumber}
+
+  If ($Update) {
+      Write-Output "Update found: $($Update.Title)"
+      $Session = New-Object -ComObject Microsoft.Update.Session
+      $Downloader = $Session.CreateUpdateDownloader()
+      $Downloader.Updates = $Update
+      $Downloader.Download()
+      $Installer = New-Object -ComObject Microsoft.Update.Installer
+      $Installer.Updates = $Update
+      $Installer.Install()
+
+      If ($Result.RebootRequired) {
+        Write-Output "Reboot required to complete the installation of the update."
+      }
+  } Else {
+      Write-Output "No update found with KB number: $KBNumber"
+  }
+
 }
