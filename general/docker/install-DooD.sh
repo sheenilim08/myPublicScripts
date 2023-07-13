f4302a53cd6ee900c5702567ccd092d0936ec2a0
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-07-14 06:17:36 +0800

    added docker install on host

--- general/docker/install-DooD.sh
+++ general/docker/install-DooD.sh
@@ -7,6 +7,26 @@ else
     exit 1
 fi
 
+echo "Checking if Docker is installed"
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker is installed"
+else
+    echo "Docker is not installed. Installing Docker on Host."
+
+    apt-get update
+    apt-get install ca-certificates curl gnupg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+    echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    echo "Finished Installing Docker"
+    docker --version
+fi
+
 if ! docker build -t customjenkins .; then
     echo "An error occurred while building the Docker image"
     exit 1
