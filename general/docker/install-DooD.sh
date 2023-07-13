8e43ff73846edf46b59d4db043002a9822e48ca8
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-07-14 06:20:05 +0800

    updated echo statements for visual reference

--- general/docker/install-DooD.sh
+++ general/docker/install-DooD.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ $(lsb_release -rs) == "20.04" && $(lsb_release -is) == "Ubuntu" ]]; then
-    echo "This machine is running Ubuntu 20.04"
+    echo "This machine is running Ubuntu 20.04. Resuming."
 else
     echo "This machine is not running Ubuntu 20.04, you have issues with docker build."
     exit 1
 fi
 
-echo "Checking if Docker is installed"
+echo "Checking if Docker is installed. Resuming."
 if [ -x "$(command -v docker)" ]; then
-    echo "Docker is installed"
+    echo "Docker is installed."
 else
     echo "Docker is not installed. Installing Docker on Host."
 
@@ -33,10 +33,11 @@ if ! docker build -t customjenkins .; then
 fi
 
 if [ "$(docker volume ls | grep jenkins_home)" ]; then
-    echo "Volume jenkins_home already exists"
+    echo "Volume jenkins_home already exists. Resuming."
 else
     docker volume create jenkins_home
     echo "Volume jenkins_home created"
 fi
 
+echo "Creating jenkins container."
 docker run -p 8079:8080 -d --name jenkins -p 50000:50000 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker customjenkins
