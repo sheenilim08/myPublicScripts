5008f3f2dfbcabeabbddc84c30b3d285b6d60598
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-07-13 10:40:19 +0800

    Added custom jenkins install

---
+++ general/docker/install-DooD.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker build -t customjenkins .
+
+if [ "$(docker volume ls | grep jenkins_home)" ]; then
+    echo "Volume jenkins_home already exists"
+else
+    docker volume create jenkins_home
+    echo "Volume jenkins_home created"
+fi
+
+docker run -p 8079:8080 -d --name jenkins -p 50000:50000 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker customjenkins
