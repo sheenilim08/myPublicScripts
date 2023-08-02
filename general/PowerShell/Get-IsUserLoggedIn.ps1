77ff805aa63bb4b423d743588f31b25bd8d7a5e6
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-08-02 08:58:01 +0800

    Added a script to check if a user is logged in to a local computer.

---
+++ general/PowerShell/Get-IsUserLoggedIn.ps1
@@ -0,0 +1,36 @@
+# The code is refactored from "omniomi" post on the link below.
+# https://superuser.com/questions/1186568/powershell-get-active-logged-in-user-in-local-machine
+
+$userToLookfor = ""
+$hasFoundUser = $false
+
+$Users = query user 2>&1
+
+function main() {
+  $Users = $Users | ForEach-Object {
+      (($_.trim() -replace ">" -replace "(?m)^([A-Za-z0-9]{3,})\s+(\d{1,2}\s+\w+)", '$1  none  $2' -replace "\s{2,}", "," -replace "none", $null))
+  } | ConvertFrom-Csv
+
+
+
+  foreach ($User in $Users)
+  {
+      $currentUser = [PSCustomObject]@{
+          ComputerName = $Computer
+          Username = $User.USERNAME
+          SessionState = $User.STATE.Replace("Disc", "Disconnected")
+          SessionType = $($User.SESSIONNAME -Replace '#', '' -Replace "[0-9]+", "")
+      } 
+
+      if ($currentUser.Username.tolower() -eq $userToLookfor.tolower()) {
+        $hasFoundUser = $true
+        Write-Output "User $($userToLookfor) is logged in."
+        return 0;
+      }
+  }
+
+  if (-not $hasFoundUser) {
+    Write-Output "User is not logged in."
+    return 1;
+  }
+}
