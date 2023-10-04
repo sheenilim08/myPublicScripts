08616934ebc202cf4127f9776f6a65209e8239c2
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-10-04 09:10:18 +0800

    Get inaccessible folders script

---
+++ general/PowerShell/Get-InaccessibleFolders.ps1
@@ -0,0 +1,22 @@
+param($folderPath)
+
+$inaccessibleFolders = @()
+
+#Get-ChildItem -Path $folderPath -Directory | ForEach-Object {
+$childFolders = Get-ChildItem -Path $folderPath -Directory | Sort-Object FullName
+foreach ($currentFolder in $childFolders) {
+    try {
+        New-Item -Path $currentFolder.FullName -Name "testfile.txt" -Force -ItemType File -ErrorAction Stop
+        Remove-Item -Path "$($currentFolder.FullName)\testfile.txt"
+    } catch {
+        $thisFolder = New-Object -TypeName Object
+        $thisFolder | Add-Member -MemberType NoteProperty -Name "Path" -Value $currentFolder.FullName
+        $inaccessibleFolders += $thisFolder
+    }
+}
+
+if ($inaccessibleFolders.Count -eq 0) {
+    Write-Output "There are no folders that you cannot access."
+}
+
+$inaccessibleFolders | FT -AutoSize -Wrap
