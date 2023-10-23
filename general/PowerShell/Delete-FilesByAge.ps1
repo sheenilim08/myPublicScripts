f8a942643bc476c3ed8b539188be0dff5cd8adeb
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-10-23 11:03:38 +0800

    Added Delete Files by AGe

---
+++ general/PowerShell/Delete-FilesByAge.ps1
@@ -0,0 +1,29 @@
+param(
+  [Parameter(HelpMessage="The path and file filter to delete")]
+  $fileFilter = "C:\*.xml",
+  
+  [Parameter(HelpMessage="The minimum age to delete.")]
+  $ageToDelete = 91
+)
+
+function main() {
+  $filteredItems = Get-ChildItem -Path $fileFilter
+
+  $quafliedForDeletion = @()
+  foreach ($currentItem in $filteredItems) {
+    $age = (Get-Date).Subtract($currentItem.CreationTime).Days
+    if ($age -ge $ageToDelete) {
+      $qualifiedItem = New-object -TypeName PSObject
+      $qualifiedItem | Add-Member -MemberType NoteProperty -Name "Path" -Value $currentItem.FullName
+      $qualifiedItem | Add-Member -MemberType NoteProperty -Name "Age" -Value $age
+      $qualifiedItem | Add-Member -MemberType NoteProperty -Name "DateCreation" -Value $currentItem.CreationTime
+
+      $quafliedForDeletion += $qualifiedItem
+    }
+  }
+  
+  
+  $quafliedForDeletion | Sort-Object Age | FT Path, Age, DateCreation -AutoSize
+}
+
+main
