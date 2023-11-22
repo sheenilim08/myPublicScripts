fb40208f8902d17520607abc6da9b63523c09078
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-11-22 13:23:17 +0800

    corrected condition for test path

--- general/PowerShell/Backup-IMConfig.ps1
+++ general/PowerShell/Backup-IMConfig.ps1
@@ -10,13 +10,17 @@ function Unzip
 function main() {
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-  Invoke-WebRequest "https://arcserve.file.force.com/sfc/dist/version/download/?oid=00D36000000JVmG&ids=0683600000DCuCHAA1&d=%2Fa%2F36000000DFQK%2F1kv.e1ULZhByGtQEGKLqUUxj6oeC49OvGS1vyUVg3d4&operationContext=DELIVERY&viewId=05HHq00000J7kq4MAB&dpt=" -OutFile "$env:userprofile\IMBackupConfig.zip"
+  if ( -not (Test-Path -Path $env:userprofile\IMBackupConfig-Tool )) {
+    mkdir $env:userprofile\IMBackupConfig-Tool 
+  }
 
-  Unzip -zipfile $env:userprofile\IMBackupConfig.zip -outpath $env:userprofile\IMBackupConfig
-  cd $env:userprofile\IMBackupConfig
+  cd $env:userprofile\IMBackupConfig-Tool
+
+  Invoke-WebRequest "https://raw.githubusercontent.com/sheenilim08/myPublicScripts/master/general/PowerShell/common/IMBackup.cmd" -OutFile IMBackup-Tool.cmd
+
+  # Unzip -zipfile $env:userprofile\IMBackupConfig-Tool.zip -outpath $env:userprofile\IMBackupConfig-Tool
   
-  Get-Content IMBackup.cmd | Select-String -pattern "pause" -notmatch | Out-File IMBackup-nopause.cmd
-  .\IMBackup-nopause.cmd
+  .\IMBackup-Tool.cmd
 
   $currentDate = Get-Date
 
