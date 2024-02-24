78aeeebe25716ada5bd4a10e5de656e437b81d07
Sheen Ismhael A. Lim <slim@modonetworks.net>
2024-02-24 17:22:50 -0600

    saved new-adgroup result to variable

--- general/AD/Add-ADGroup.ps1
+++ general/AD/Add-ADGroup.ps1
@@ -31,9 +31,9 @@ function main() {
   foreach ($companyLine in $csvFile) {
     Write-Output "Creating group for $($companyLine.GroupName)"
 
-    New-ADGroup -Path $companyLine.OUDN -Name $companyLine.GroupName -DisplayName $companyLine.GroupName -SamAccountName $companyLine.SamAccountName -Description $companyLine.GroupDescription -GroupCategory $companyLine.GroupType -GroupScope $companyLine.GroupScope
+    $thisGroup = New-ADGroup -Path $companyLine.OUDN -Name $companyLine.GroupName -DisplayName $companyLine.GroupName -SamAccountName $companyLine.SamAccountName -Description $companyLine.GroupDescription -GroupCategory $companyLine.GroupType -GroupScope $companyLine.GroupScope
 
-    Set-ADGroup -Identity $companyLine.GroupName -Replace @{info="$($companyLine.Notes)"}
+    $thisGroup | Set-ADGroup -Replace @{info="$($companyLine.Notes)"}
   }
 
   # Write-Output $csvFile
