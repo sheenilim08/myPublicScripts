5715a61cf70d6f76154bdd8d8bfcc8d767bb8446
Sheen Ismhael A. Lim <slim@modonetworks.net>
2024-02-24 18:04:23 -0600

    baseline encrypted volume unlock script

---
+++ general/PowerShell/AD/Bitlocker/Unlock-BitlockerVolumes.ps1
@@ -0,0 +1,39 @@
+params(
+  $smtpServer,
+  [switch]$ssl,
+  [switch]$unlock,
+  $kbLink
+)
+
+function Send-EmailNotification {
+  params(
+    $emailBody
+  )
+}
+
+function main() {
+  $encryptedVolumes = [Array]@(Get-BitLockerVolume)
+
+  $messageLog = ""
+  foreach ($volume in $encryptedVolumes) {
+    if ($volume.LockStatus -eq "Locked") {
+      $currentDate = Get-Date
+      $timeStamp = "$($currentDate.ToLongDateString()) $($currentDate.ToLongTimeString()): "
+      $messageLog += "$($timeStamp) Server $($volume.ComputerName) volume $($volume.MountPoint) is Encrypted and Locked.`n"
+
+      if ($unlock) {
+        try {
+          Unlock-BitLocker -MountPoint $volume.MountPoint
+          $messageLog += "$($timeStamp) Server $($volume.ComputerName) volume $($volume.MountPoint) is now unlocked. No further action is required. `n"
+        } catch {
+          $messageLog += "$($timeStamp) Unable to unlock the volume $($volume.MountPoint) on Server $($volume.ComputerName). Please check $($kbLink)`n"
+        } finally {
+          Send-EmailNotification -emailBody $messageLog
+        }
+      }
+    }
+  }
+}
+
+$ErrorActionPreference = 'Continue'
+main
