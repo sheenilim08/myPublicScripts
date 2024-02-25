748d1831dae03219aefd63219c9d805b89d82be5
Sheen Ismhael A. Lim <slim@modonetworks.net>
2024-02-24 18:11:50 -0600

    custom bitlocker notification message

--- general/PowerShell/AD/Bitlocker/Unlock-BitlockerVolumes.ps1
+++ general/PowerShell/AD/Bitlocker/Unlock-BitlockerVolumes.ps1
@@ -18,7 +18,7 @@ function main() {
   foreach ($volume in $encryptedVolumes) {
     if ($volume.LockStatus -eq "Locked") {
       $currentDate = Get-Date
-      $timeStamp = "$($currentDate.ToLongDateString()) $($currentDate.ToLongTimeString()): "
+      $timeStamp = "$($currentDate.ToLongDateString()) $($currentDate.ToLongTimeString()):"
       $messageLog += "$($timeStamp) Server $($volume.ComputerName) volume $($volume.MountPoint) is Encrypted and Locked.`n"
 
       if ($unlock) {
@@ -26,13 +26,13 @@ function main() {
           Unlock-BitLocker -MountPoint $volume.MountPoint
           $messageLog += "$($timeStamp) Server $($volume.ComputerName) volume $($volume.MountPoint) is now unlocked. No further action is required. `n"
         } catch {
-          $messageLog += "$($timeStamp) Unable to unlock the volume $($volume.MountPoint) on Server $($volume.ComputerName). Please check $($kbLink)`n"
-        } finally {
-          Send-EmailNotification -emailBody $messageLog
+          $messageLog += "$($timeStamp) Unable to unlock the volume $($volume.MountPoint) on Server $($volume.ComputerName). Please check $($kbLink)`n`n"
         }
       }
     }
   }
+
+  Send-EmailNotification -emailBody $messageLog
 }
 
 $ErrorActionPreference = 'Continue'
