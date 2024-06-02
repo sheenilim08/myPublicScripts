b63da9604df8789dcbc6ba4e58cc8365278e47ca
Sheen Ismhael Lim <sheenismhael.lim@outlook.ph>
2024-06-02 09:10:48 +0000

    added script to get all tickets in professional and zenith boards to me

---
+++ general/ConnectWise/Assign-TicketsToMe.ps1
@@ -0,0 +1,65 @@
+# Code Wrapper used is from
+# https://github.com/christaylorcodes/ConnectWiseManageAPI
+
+
+param(
+    $CWEndpoint, 
+    $CWCompanyID,
+    $CWPublicKey,
+    $CWPrivateKey.
+    $CWClientID
+)
+
+$CWMConnectionInfo = @{
+  # This is the URL to your manage server.
+  Server      = $CWEndpoint
+  # This is the company entered at login
+  Company     = $CWCompanyID
+  # Public key created for this integration
+  pubkey      = $CWPublicKey
+  # Private key created for this integration
+  privatekey  = $CWPrivateKey
+  # Your ClientID found at https://developer.connectwise.com/ClientID
+  clientid    = $CWClientID
+}
+
+Write-Output "Connecting to CW Endpoint $($CWMConnectionInfo))"
+Connect-CWM @CWMConnectionInfo -Force
+
+Write-Output "Querying Unassigned Tickets - Professional Services..."
+$unassignedProfTicket = @(Get-CWMTicket -condition '((status/name = "New" or status/name = "New \(email connector\)") and board/name = "Professional Services" and resources = null)')
+
+$unassignedProfTicket | Foreach-Object {
+    $ticketID = $_.id
+
+    $ticketOwner = @{
+        ID = $ticketID
+        Operation = 'replace'
+        Path = 'owner'
+        Value = @{id=268}
+    }
+
+    Write-Output "Updating Resources and Ticket Owner for $($ticketID) $($_.summary)"
+    Update-CWMTicket @ticketOwner
+#    New-CWMScheduleEntry -member @{identifier = "SLim"} -objectId $ticketID -type @{id=4}
+}
+
+Write-Output "Querying Unassigned Tickets - Zenith..."
+$unassignedZenithfTicket = @(Get-CWMTicket -condition '((status/name = "New" or status/name = "New \(email connector\)") and board/name = "Zenith" and resources = null and (summary not contains "desktop"))')
+
+$unassignedZenithfTicket | Foreach-Object {
+    $ticketID = $_.id
+
+    $ticketOwner = @{
+        ID = $ticketID
+        Operation = 'replace'
+        Path = 'owner'
+        Value = @{id=268}
+    }
+
+    Write-Output "Updating Resources and Ticket Owner for $($ticketID) $($_.summary)"
+    Update-CWMTicket @ticketOwner
+#    New-CWMScheduleEntry -member @{identifier = "SLim"} -objectId $ticketID -type @{id=4}
+}
+
+Disconnect-CWM
