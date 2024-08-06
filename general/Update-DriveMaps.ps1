b663c3344ddfdbba6dab9c15fd669f71513c3f43
Sheen Lim <sheenismhael.lim@outlook.com>
2024-08-06 18:13:22 +0800

    fixed drive map issues

--- general/Update-DriveMaps.ps1
+++ general/Update-DriveMaps.ps1
@@ -7,26 +7,36 @@ $OldServer = $OldServer.ToLower()
 $NewServer = $NewServer.ToLower()
 
 $driveHistory = @()
- 
-Get-WmiObject win32_logicaldisk | Where-Object {$_.ProviderName -ne $null -and $_.ProviderName.ToLower() -like "$($OldServer)*" } | ForEach-Object { 
-    $Name = (($_.DeviceID) -replace ":", "")
-    $NewRoot = $_.ProviderName.ToLower().Replace($OldServer, $NewServer)
+
+$currentLoggedInUser = Get-ItemProperty -Path HKCU:"Volatile Environment"
+
+Write-Output "Getting the Network Drives for $($currentLoggedInUser.USERDOMAIN)\$($currentLoggedInUser.USERNAME)"
+
+$driveMaps = Get-WmiObject win32_logicaldisk | Where-Object {$_.ProviderName -ne $null -and $_.ProviderName.ToLower() -like "$($OldServer)*" } 
+
+Write-Output "Checking Mapped Drives"
+for ($i=0; $i -lt $driveMaps.Length; $i++) {
+    $currentDrive = $driveMaps[$i]
+    Write-Output "Checking $($currentDrive.DeviceID)$($currentDrive.ProviderName)"
+
+    $Name = $currentDrive.DeviceID.Replace(":", "")
+    $NewRoot = $currentDrive.ProviderName.ToLower().Replace($OldServer, $NewServer)
 
     Write-Output "Name: $($Name)"
     Write-Output "NewRoot: $($NewRoot)"
 
     if (!$softRun) {
-        Write-Output "Updating drive map $($Name): $($_.ProviderName) -> $($NewRoot)"
+        Write-Output "Updating drive map $($Name): $($currentDrive.ProviderName) -> $($NewRoot)"
         Get-PSDrive $Name | Remove-PSDrive -Force -whatif
         New-PSDrive $Name -PSProvider FileSystem -Root $NewRoot -WhatIf
     }
 
     $driveObj = New-Object -TypeName PSObject
-    $driveObj | Add-Member -MemberType NoteProperty -Name DriveLetter -Value $_.DeviceID
-    $driveObj | Add-Member -MemberType NoteProperty -Name OldDriveMap -Value $_.ProviderName
+    $driveObj | Add-Member -MemberType NoteProperty -Name DriveLetter -Value $currentDrive.DeviceID
+    $driveObj | Add-Member -MemberType NoteProperty -Name OldDriveMap -Value $currentDrive.ProviderName
     $driveObj | Add-Member -MemberType NoteProperty -Name NewDriveMap -Value $NewRoot
 
     $driveHistory += $driveObj
 }
 
-$driveHistory
+$driveHistory | FT -AutoSize
