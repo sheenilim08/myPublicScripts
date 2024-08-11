30512a79c911735d44f627fce588f16bf48e4194
Sheen Lim <sheenismhael.lim@outlook.com>
2024-08-11 13:53:40 +0800

    update shortcut references

--- general/Update-DriveMaps.ps1
+++ general/Update-DriveMaps.ps1
@@ -3,8 +3,8 @@ $NewServer = '\\epic-dc01\'
 
 $softRun = $false
 
-$OldServer = $OldServer.ToLower()
-$NewServer = $NewServer.ToLower()
+$OldServer = $OldServer.ToLower() # Server name must start with '\\' as in '\\oldservername'
+$NewServer = $NewServer.ToLower() # Server name must start with '\\' as in '\\newservername'
 
 $driveHistory = @()
 
@@ -22,13 +22,10 @@ for ($i=0; $i -lt $driveMaps.Length; $i++) {
     $Name = $currentDrive.DeviceID.Replace(":", "")
     $NewRoot = $currentDrive.ProviderName.ToLower().Replace($OldServer, $NewServer)
 
-    Write-Output "Name: $($Name)"
-    Write-Output "NewRoot: $($NewRoot)"
-
+    Write-Output "Updating drive map $($Name): $($currentDrive.ProviderName) -> $($NewRoot)"
     if (!$softRun) {
-        Write-Output "Updating drive map $($Name): $($currentDrive.ProviderName) -> $($NewRoot)"
-        Get-PSDrive $Name | Remove-PSDrive -Force -whatif
-        New-PSDrive $Name -PSProvider FileSystem -Root $NewRoot -WhatIf
+        Get-PSDrive $Name | Remove-PSDrive -Force
+        New-PSDrive $Name -PSProvider FileSystem -Root $NewRoot
     }
 
     $driveObj = New-Object -TypeName PSObject
@@ -39,4 +36,42 @@ for ($i=0; $i -lt $driveMaps.Length; $i++) {
     $driveHistory += $driveObj
 }
 
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$desktopShortcutItems = Get-Item -Path "$($desktopPath)\*.lnk"
+
+$updatedShortcuts = @()
+
+Write-Host "Updating Shortcut Files"
+for ($i=0; $i -lt $desktopShortcutItems.Length; $i++) {
+    $currentShortcutFile = $desktopShortcutItems[$i]
+    
+    $shell = New-Object -COM WScript.Shell
+    $shortcut = $shell.CreateShortcut($currentShortcutFile)  ## Open the lnk
+    
+    if ($shortcut.TargetPath -like "$($OldServer)*") {
+        $newTargetPath = $shortcut.TargetPath.ToLower().Replace($OldServer, $NewServer)
+        $renamedOldFileName = "Old $($desktopShortcutItems[$i].name)"
+        $backupFile = "$($desktopPath)\$($renamedOldFileName)"
+
+        $shotcutObj = New-Object -TypeName PSObject
+        $shotcutObj | Add-Member -MemberType NoteProperty -Name Name -Value $currentShortcutFile.name
+        $shotcutObj | Add-Member -MemberType NoteProperty -Name OldPath -Value $shortcut.TargetPath
+        $shotcutObj | Add-Member -MemberType NoteProperty -Name NewPath -Value $newTargetPath
+        $shotcutObj | Add-Member -MemberType NoteProperty -Name BackupFile -Value $renamedOldFileName
+
+        Write-Host "Updating desktop item: $($desktopShortcutItems[$i].name)"
+
+        if (!$softRun) {
+            Copy-Item $desktopShortcutItems[$i] $backupFile  ## Create a backup copy of the .lnk file.
+
+            $shortcut.TargetPath =  $newTargetPath ## Make changes
+            $shortcut.Save()  ## Save
+        }
+
+        $updatedShortcuts += $shotcutObj
+    }
+
+}
+
 $driveHistory | FT -AutoSize
+$updatedShortcuts | FT -AutoSize
