1eb723ac10b39c36585aacdf4008c3ae7d0b2e14
Sheen Lim <sheenismhael.lim@outlook.com>
2024-08-11 17:40:39 +0800

    Updated Powershell Install Script

--- general/PowerShell/Install-PowerShell.ps1
+++ general/PowerShell/Install-PowerShell.ps1
@@ -1,4 +1,4 @@
-param($msiInstaller="https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi")
+param($msiInstaller="https://github.com/PowerShell/PowerShell/releases/download/v7.4.4/PowerShell-7.4.4-win-x64.msi")
 
 function main() {
   Write-Output "Downloading $($msiInstaller)"
@@ -7,12 +7,16 @@ function main() {
   Write-Output "Unblocking Installer."
   Unblock-File .\PowerShellInstaller.msi
 
-  $knownHashValue= "6DEFE662DD9E323113E8E683F604031D1E726615FB8E102C048FF52C6E9FD1E4"; # hashvalue for PowerShell-7.4.2-win-x64.msi - SHA256
+  #$knownHashValue= "6DEFE662DD9E323113E8E683F604031D1E726615FB8E102C048FF52C6E9FD1E4"; # hashvalue for PowerShell-7.4.2-win-x64.msi - SHA256
+  $knownHashValue = "C755A79759AD5DAA5F76A855ABB46BE1B9CE86616607138FEF5B02EC4BFAE643"; # hashvalue for PowerShell-7.4.4-win-x64.msi - SHA256
+  $installerSHA256 = $(Get-FileHash -Path .\PowerShellInstaller.msi -Algorithm "SHA256").HASH
 
-  $installerSHA256 = $(Get-FileHash -Path $msiInstaller -Algorithm "SHA256").HASH.ToLower()
-
-  if ($installerSHA256 -eq $sha1Value) {
-    .\PowerShellInstaller.msi /qn /norestart
+  if ($installerSHA256 -eq $knownHashValue) {
+    #.\PowerShellInstaller.msi /qn /norestart
+    Start-Process msiexec.exe -ArgumentList "/i PowerShellInstaller.msi /qn /norestart /lev ps744_install.log" -wait
+    Write-Host "Installer is running in the background in silent mode."
+  } else {
+    Write-Host "Installer HASH did not match expected HASH value. Exiting."
   }
 }
 
