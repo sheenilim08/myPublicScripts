53eda90f5b5e635f6eeab14f22748415ef25d215
Sheen Lim <sheenismhael.lim@outlook.com>
2024-08-23 05:30:15 +0800

    check-rebootpending to be just 1 function

--- general/PowerShell/WindowsUpdate/Check-RebootPending.ps1
+++ general/PowerShell/WindowsUpdate/Check-RebootPending.ps1
@@ -1,24 +1,28 @@
 # Reference: https://theautomationcode.com/how-to-detect-if-a-server-reboot-is-pending/
 
-$isRebootPending =  $false
-if(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing" -Name "RebootPending" -EA Ignore) {
-    $isRebootPending = $true
-    Write-Output "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending is present and needing intervention."
+function main() {
+    $isRebootPending =  $false
+    if(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing" -Name "RebootPending" -EA Ignore) {
+        $isRebootPending = $true
+        Write-Output "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending is present and needing intervention."
+    }
+
+    if(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name "RebootRequired" -EA Ignore) {
+        $isRebootPending = $true
+        Write-Output "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootPending is present and needing intervention."
+    }
+
+    if(Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name "PendingFileRenameOperations" -EA Ignore){
+        $isRebootPending = $true
+        Write-Output "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\PendingFileRenameOperations is present and needing intervention."
+    }
+
+    if($isRebootPending){
+        Write-Output "Server Reboot is required"
+        Write-Output "Check the registry item values mentioned."
+    } else {
+        Write-Output "Server Reboot is not required."
+    }
 }
 
-if(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name "RebootRequired" -EA Ignore) {
-    $isRebootPending = $true
-    Write-Output "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootPending is present and needing intervention."
-}
-
-if(Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name "PendingFileRenameOperations" -EA Ignore){
-    $isRebootPending = $true
-    Write-Output "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\PendingFileRenameOperations is present and needing intervention."
-}
-
-if($isRebootPending){
-    Write-Output "Server Reboot is required"
-    Write-Output "Check the registry item values mentioned."
-} else {
-    Write-Output "Server Reboot is not required."
-}
+main
