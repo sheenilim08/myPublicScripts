5b1dfdaebbf677e633efd9d7a616456f8d9a7190
Sheen Ismhael A. Lim <slim@modonetworks.net>
2025-04-07 19:03:23 -0500

    added console logs

--- Azure/Partner/Get-AllTenants.ps1
+++ Azure/Partner/Get-AllTenants.ps1
@@ -13,10 +13,12 @@ Write-Host "Login to your Partner Account."
 Connect-MSOLService
 
 function main() {
+    Write-Host "Getting all Tenants IDs."
     $tenantIds = Get-MsolPartnerContract -All
     $tenantInfo = @()
 
     for ($i = 0; $i -lt $tenantIds.Count; $i++) {
+        Write-Host "Resolving Tenant id $($tenantIds[$i].TenantId)."
         $thisTenant = Get-MsolPartnerInformation -TenantId $tenantIds[$i].TenantId
         $tenantData = New-Object -TypeName PSObject
         $tenantData | Add-Member -MemberType NoteProperty -Name "TenantId" -Value $tenantIds[$i].TenantId
